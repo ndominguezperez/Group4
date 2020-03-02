88b0fc1578cf790e4d1cbdab206034d43c81c3ae
hecyebesdelpino <hecyebesdelpino@10.60.58.53>
2020-03-02 18:16:11 +0100

    prueba

--- src/sample/Sample.java
+++ src/sample/Sample.java
@@ -1,13 +1,9 @@
 package sample;
 
 public class Sample {
-	#System.out.println(
-			1. "Select your "
-			"Introduzca el nombre de usuario"
-			#--> Nombre de usuario = Funcion para introducir texto
-			 	"Introduzca la contraseña"
-			#--> Funcion para introducir texto
-			
-			if 
-			);
+	
+}
+
+public static void main () {
+	System.out.println("Hola chicos");
 }
