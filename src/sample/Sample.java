bbff966a116f17e238cc5d8a553a62306d97b152
hecyebesdelpino <hecyebesdelpino@10.60.58.53>
2020-03-02 18:15:27 +0100

    Mensaje de prueba

--- src/sample/Sample.java
+++ src/sample/Sample.java
@@ -1,5 +1,13 @@
 package sample;
 
 public class Sample {
-
+	#System.out.println(
+			1. "Select your "
+			"Introduzca el nombre de usuario"
+			#--> Nombre de usuario = Funcion para introducir texto
+			 	"Introduzca la contraseña"
+			#--> Funcion para introducir texto
+			
+			if 
+			);
 }
