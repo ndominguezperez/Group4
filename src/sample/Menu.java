f8c89e81dfb32d6545faff2daac5c1cbbde645d0
hecyebesdelpino <hecyebesdelpino@10.60.58.24>
2020-03-06 20:10:45 +0100

    Menu nuevo

--- src/sample/Menu.java
+++ src/sample/Menu.java
@@ -1,7 +1,7 @@
 package sample;
 import java.io.*;
 
-public class menu {
+public class Menu {
 	public static void main (String args[]) {
 	System.out.println("Choose your identity");
 	System.out.println("1.Doctor");
