d342409608c9e935ce72a896476d4af8dc6ee332
carmen <carmen@10.60.59.97>
2020-03-09 18:10:49 +0100

    ADMIN

--- src/sample/Admin_Staff.java
+++ src/sample/Admin_Staff.java
@@ -5,5 +5,53 @@ public class Admin_Staff {
 	String name;
 	Fecha dob;
 	float salary;
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Admin_Staff other = (Admin_Staff) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Admin_Staff [id=" + id + ", name=" + name + ", dob=" + dob + ", salary=" + salary + "]";
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Fecha getDob() {
+		return dob;
+	}
+	public void setDob(Fecha dob) {
+		this.dob = dob;
+	}
+	public float getSalary() {
+		return salary;
+	}
+	public void setSalary(float salary) {
+		this.salary = salary;
+	}
 	
 }
