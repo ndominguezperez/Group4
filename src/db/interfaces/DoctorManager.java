6f18666ca4510e5fd1e764b7e4555107ae18fc70
carmen <carmen@192.168.1.39>
2020-03-30 15:55:45 +0200

    DocManager+PatientManager

--- src/db/interfaces/DoctorManager.java
+++ src/db/interfaces/DoctorManager.java
@@ -6,18 +6,19 @@ import java.util.List;
 import pojos.*;
 
 public interface DoctorManager {
-	public Patient getPatientById(int id);
 	public List<Patient> listAllPatients();
-	public List<Patient> searchPatientByName(String name);
-	public List<Patient> searchPatientBySurname(String surname);
-	public List<Appointment> viewSchedule();
-	public boolean modifyAppointment(int id);
-	public Examination viewExamination(int id);
-	public boolean modifyExamination(int id); 
 	public boolean createTreatmen(int id);
 	public Treatment viewTreatment(int id);
 	public boolean modifyTreatment(int id); 
-	public boolean setUpMachine(int id, Date date);
 	public void addNewDoctor(Doctor doctor);
+	public List<Appointment> viewSchedule();
+	public boolean setUpAppointment (Appointment appointment);
+	public boolean modifyAppointment (Appointment appointment);
+	public Appointment getAppointmentById(int id);
+	public List<Appointment> searchAppoitmentByDate(Date date);
+	public Examination viewExamination(int id);
+	public boolean modifyExamination(int id); 
+	public List <Result> viewResults();
+
 
 }
--- src/db/interfaces/PatientManager.java
+++ src/db/interfaces/PatientManager.java
@@ -1,12 +1,25 @@
 package db.interfaces;
 
+import java.sql.Date;
 import java.util.List;
 
 import pojos.*;
 
 public interface PatientManager {
-	public List<Appointment> viewSchedule();
-	public List <Result> viewResults();
+	public List<Patient> listAllPatients();
 	public void addNewPatient(Patient patient);
-	
+	public Patient getPatient(int patientId);
+	public List<Patient> searchByName(String name);
+	public List<Patient> searchBySurname(String surname);
+	public boolean setUpMachine(int id, Date date);
+	public List<Appointment> viewSchedule();
+	public boolean setUpAppointment (Appointment appointment);
+	public boolean modifyAppointment (Appointment appointment);
+	public Appointment getAppointmentById(int id);
+	public List<Appointment> searchAppoitmentByDate(Date date);
+	public List <Result> viewResults();
+	public Examination viewExamination(int id);
+	public boolean modifyExamination(int id); 
+	public Treatment viewTreatment(int id);
+	public boolean modifyTreatment(int id); 
 }
