80a2e38bb899d31eb8b82c1a0940881af2e1dd76
hecyebesdelpino <hecyebesdelpino@localhost>
2020-03-30 19:00:09 +0200

    Menu de hec

--- src/ui/Menu.java
+++ src/ui/Menu.java
@@ -1,33 +1,66 @@
- package ui;
+package ui;
+import pojos.*;
+ 
 
 //import utilities.Utilities;
 
 
 public class Menu {
-	private static void doctorSubMenu (int doctor_id) {
-		while (true) {
-			System.out.println("Select what you want to do");
-			System.out.println("\n\t1.Schedule");		
-			System.out.println("\n\t2.Patient");
-			String read;
-			read= utilities.Utilities.read();
-	        int option = Integer.parseInt(read);
+	private static void doctorSubMenu (int doctor_id) {		
+		int option;
+			do {
+				System.out.println("Select what you want to do");
+				System.out.println("\n\t1.Schedule");		
+				System.out.println("\n\t2.Patient");
+				System.out.println("\n\t3.Exit");
+				option = utilities.Exceptions.checkInt();
+			
 	        switch (option) {
 	        	case 1: //View schedule
-	        		utilities.Utilities.getDoctorSchedule();
+	        		System.out.println("\n\n\tYour schedule Doctor:%d \n" + doctor_id);
+	        		utilities.Utilities.getDoctorSchedule(); //Tener que meter el doctor_id
+	        		utilities.Utilities.read();
 	        		break;
 	        		
 	        	case 2: //Patient	
-	        		//Which patient you want?
-	        		utilities.Utilities.getPatientById();
-	        		
-	        		//
-	        		
+	        		System.out.println("\nHow do you want to search your patient?");
+	        		System.out.println("\n\t1.By name");
+	        		System.out.println("\n\t2.By surname");
+	        		System.out.println("\n\t3.By id");
+	        		System.out.println("\n\t4.I don´t want to search a patient");
+	        		int patient_option;
+	        		do {
+	        		patient_option = utilities.Exceptions.checkInt();}
+	        		while (patient_option != 1 || patient_option != 2|| patient_option != 3 || patient_option != 4);
+	        		switch (patient_option) {
+	        			case 1: 
+	        				System.out.println("Patient searched by name");
+	        				utilities.Utilities.searchPatientByName();
+	        				utilities.Utilities.read();
+	        				break;
+	        			case 2: 
+	        				System.out.println("Patient searched by surname");
+	        				utilities.Utilities.searchPatientBySurname();
+	        				utilities.Utilities.read();
+	        				break;
+	        			case 3: 
+	        				System.out.println("\nPatient searched by id");
+	        				utilities.Utilities.getPatientById();
+	        				utilities.Utilities.read();
+	        				break;
+	        			case 4: 
+	        				utilities.Utilities.read();
+	        				break;
+	        		}
 	        		break;
-	        
+	        	case 3: 
+	        		break;	        		
+	        		
 	        }
 	        
+	        }while (option !=1 || option !=2 || option != 3);
+	        
 		}
-	}
-}
+	
 
+}
