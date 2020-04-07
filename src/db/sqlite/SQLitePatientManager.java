536a63aa6f4bcacdb0a7b62e6d2c2f0e1da25700
gustavo <gustavo@localhost>
2020-04-07 16:43:40 +0200

    db

--- src/db/sqlite/SQLitePatientManager.java
+++ src/db/sqlite/SQLitePatientManager.java
@@ -53,12 +53,57 @@ public class SQLitePatientManager implements PatientManager{
 		// TODO Auto-generated method stub
 		return null;
 	}
-	@Override
-	public Patient getPatient(int patientId) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+
 	
 	
+
+	public Patient getPatient (int patientId) {return null;}/*
+		// Get Patients
+		Patient newPatient = null;
+		try {
+			String sql = "SELECT * FROM patients AS d JOIN dogsMedicines AS dm ON d.id = dm.dogId"
+					+ " JOIN medicines AS m ON dm.medicineId = m.id" + " WHERE d.id = ?";
+			// Get the joining of three tables
+			// id, name, breed, weight, admissionDate, releaseDate, dogId, medicineId, id,
+			// name
+			// EXAMPLE
+			// 1, Lassie, Collie, 10, 2020-06-01, 2020-08-01, 1, 1, 1, Dalsi
+			// 1, Lassie, Collie, 10, 2020-06-01, 2020-08-01, 1, 2, 2, Dogmotril
+			PreparedStatement p = c.prepareStatement(sql);
+			p.setInt(1, dogId);
+			ResultSet rs = p.executeQuery();
+			List<Medicine> medicinesList = new ArrayList<Medicine>();
+			// WHEN YOU DO A JOIN WITH SQLITE YOU CANNOT USE COLUMN NAMES
+			// YOU MUST USE NUMBERS INSTEAD
+			// THIS IS BECAUSE SQLITE DOESN'T SUPPORT JOINS WITH ALIASES
+			// To avoid creating the dog several times
+			boolean dogCreated = false;
+			while (rs.next()) {
+				// If the dog has not been created
+				if (!dogCreated) {
+					// Get the dog information
+					int newDogId = rs.getInt(1);
+					String dogName = rs.getString(2);
+					String breed = rs.getString(3);
+					float weight = rs.getFloat(4);
+					Date admissionDate = rs.getDate(5);
+					Date releaseDate = rs.getDate(6);
+					// Create a new dog
+					newDog = new Dog(newDogId, dogName, breed, weight, admissionDate, releaseDate);
+					dogCreated = true;
+				}
+				// Get the medicine information
+				int medicineId = rs.getInt(9);
+				String medicineName = rs.getString(10);
+				Medicine newMedicine = new Medicine(medicineId, medicineName);
+				medicinesList.add(newMedicine);
+			}
+			newDog.setMedicines(medicinesList);
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return newDog;
+	}*/
 }
 
